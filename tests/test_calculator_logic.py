83c58e0731a5bfb4250a17910795366e539889bf
akanksha <akanksha.lede@moschip.com>
2025-05-23 15:04:03 +0530

    changed value for add and subtract

--- tests/test_calculator_logic.py
+++ tests/test_calculator_logic.py
@@ -7,11 +7,11 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from calculator import calculator
 
 def test_add_positive():
-    result = calculator(-11, 90, ['add'])
+    result = calculator(-11, 9, ['add'])
     assert result['add'] > 0, "Add result should be positive"
 
 def test_subtract_positive():
-    result = calculator(-100, -7000, ['subtract'])
+    result = calculator(100, 7000, ['subtract'])
     assert result['subtract'] > 0, "Subtract result should be positive"
 
 def test_divide_greater_equal_one():
